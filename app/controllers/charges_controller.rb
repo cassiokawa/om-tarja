cf8ca1f8c541ce49a50762790811c23e5ac7d132
cassiokawakita <cassio.kawakita@gmail.com>
2015-05-08 21:34:00 +0000

    Final version

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,8 +1,7 @@
 class ChargesController < ApplicationController  
     def create
       # Amount in cents
-      product = Product.find_by_sku("GROHACK2")
-    
+      product = Product.find(params[:product_id])
       customer = Stripe::Customer.create(
         :email => params[:stripeEmail],
         :card  => params[:stripeToken],
@@ -16,10 +15,15 @@ class ChargesController < ApplicationController
       #   :currency    => 'usd'
       # )
       
-      purchase = Purchase.create(email: params[:stripeEmail], 
-        card: params[:stripeToken], amount: product.price_in_cents, 
-        description: product.full_description, currency: 'usd',
-        customer_id: customer.id, product_id: product.id, uuid: SecureRandom.uuid)
+      purchase = Purchase.create(
+        email: params[:stripeEmail], 
+        card: params[:stripeToken], 
+        amount: @amount, 
+        description: product.full_description,
+        currency: 'usd',
+        customer_id: customer.id,
+        product_id: product.id,
+        uuid: SecureRandom.uuid)
 
     redirect_to purchase
     
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  root "pages#home"
+  root "products#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :charges
   
   # Route for showing products
-  resources :products, only: [:index]
+  resources :products, only: [:index, :show]
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
